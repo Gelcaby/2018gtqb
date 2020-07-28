35e4adf39321b693da75e4fd0c58299fe66fb87f
Gelcaby <67866078+Gelcaby@users.noreply.github.com>
2020-07-28 11:45:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	if(file_exists("config.php")) {
 		require "config.php";
 	} else {
-		define("title","MeowChat - A Public Chat System");
+		define("title","2018钢铁七班 网站");
 	};
 	if(!defined("title")){
-		define("title","MeowChat - A Public Chat System");
+		define("title","2018钢铁七班 网站");
 	};
 ?>
 <!DOCTYPE HTML>
@@ -20,13 +20,13 @@
 	<body style="background-color:#2588cb;color:#fff;text-align:center;padding:0;margin:0;overflow:hidden;" scroll="no">
 	<center>
 		<img style="margin-top:10%;height:140px;weigh:140px;background-color:white;border-radius: 15px;" src="favicon.ico"/>
-		<h1>Meow-Chat</h1>
+		<h1>2018钢铁七班</h1>
    <form class="form-signin" action="chat.php" method="post" style="width:300px;">
 		<div class="input-group">
-  <span class="input-group-addon">用户名</span>
-  <input id="username" name="username" type="text" class="form-control" minlength="3" maxlength="12" <?php if(!empty($_GET['username'])){echo 'value="'.$_GET['username'].'"';};?> required placeholder="聊天用户名"/>
+  <span class="input-group-addon">姓名</span>
+  <input id="username" name="username" type="text" class="form-control" minlength="3" maxlength="12" <?php if(!empty($_GET['username'])){echo 'value="'.$_GET['username'].'"';};?> required placeholder="请输入您的姓名"/>
   </div>
-		<button type="submit" class="button button-3d button-primary button-rounded">进入</button>
+		<button type="submit" class="button button-3d button-primary button-rounded">登录</button>
         </form>
 		</center>
 	</body>
